0159adac7f0f8a321f7043a558ab8fd29c5b649d
RuslanHam <rurham@yandex.ru>
2018-04-10 16:50:28 +0400

    sql delete pdo::attribute fetch_mode

--- libs/Model.php
+++ libs/Model.php
@@ -6,7 +6,8 @@ class Model
   function __construct()
   {
     //connect SQL
-    $this->db = new PDO(DB_TYPE.':host='.DB_HOST.'; dbname='.DB_NAME.'; charset='.DB_CHARSET, DB_USER, DB_PASS, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+    //$this->db = new PDO(DB_TYPE.':host='.DB_HOST.'; dbname='.DB_NAME.'; charset='.DB_CHARSET, DB_USER, DB_PASS, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+    $this->db = new PDO(DB_TYPE.':host='.DB_HOST.'; dbname='.DB_NAME.'; charset='.DB_CHARSET, DB_USER, DB_PASS);
   }
 
 }
